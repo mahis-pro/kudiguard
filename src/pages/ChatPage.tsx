c5550d90534c9e28fc30a416a99f0ad9ddb13fe0
Moshood Abdulhaqq Omotayo <moshoodoabdulhaqq@gmail.com>
2025-10-06 14:35:23 +0100

    [dyad] wrote 1 file(s)

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -502,10 +502,7 @@ const ChatPage = () => {
   return (
     <>
       <div className="flex flex-col flex-1">
-        <div className="flex items-center justify-between p-4 border-b border-border bg-card sticky top-0 z-10">
-          <h2 className="text-xl font-semibold text-primary">KudiGuard Chat</h2>
-          {/* Removed the Start New Chat button from here */}
-        </div>
+        {/* Removed the KudiGuard Chat header */}
         <div className="flex-1 overflow-y-auto p-4 space-y-4">
           {messages.map((msg) => (
             <div
